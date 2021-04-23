51364b1c4ae556c94fc8511b20e66db99cc30da3
arielmell0 <arielmello@rede.ulbra.br>
2021-04-23 07:48:27 -0300

    feat: :sparkles: end of the day one of the nlw05
    added routes and server listenner

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from "express"
+
+const app = express()
+
+/**
+ * Métodos
+ 
+ * GET = Buscas
+ * POST = Criar
+ * PUT = Alterar
+ * DELETE = Deletar
+ * PATCH = Alteração especifíca
+ */
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Olá mundo GET!"
+    })
+})
+
+app.post("/", (request, response) => {
+    return response.json({
+        message: "Olá mundo POST!"
+    })})
+
+app.listen(3333, () => console.log("Server is running on port 3333"))
